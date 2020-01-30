9ec05d47e8a4e7175bf0033c99bbf25fe5de715f
smperez1997 <52365447+smperez1997@users.noreply.github.com>
2020-01-30 16:19:45 -0500

    as

--- src/main/java/com/clean/app/web/models/dao/IHerramienta.java
+++
@@ -1,9 +0,0 @@
-package com.clean.app.web.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.clean.app.web.models.entities.Herramienta;
-
-public interface IHerramienta extends CrudRepository<Herramienta, Integer> {
-
-}
